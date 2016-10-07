b7afd15b88ab0a3ae0cacd1dea13b23135ed569a
ZHIYUAN-PC\Zhi Yuan <zhiyuan_1994@yahoo.com.sg>
2016-10-07 23:21:51 +0800

    update DateItem

--- src/main/java/seedu/address/model/item/DateItem.java
+++ src/main/java/seedu/address/model/item/DateItem.java
@@ -3,12 +3,21 @@ package seedu.address.model.item;
 import java.util.Date;
 
 public abstract class DateItem implements Comparable<DateItem> {
-    protected Date mainDateToOrderBy;
+    protected Name itemName;
+    protected Date dateToOrderBy;
     
     /**
      * Orders the Date objects in ascending order  
      */
     public int compareTo(Date otherDate) {
-        return mainDateToOrderBy.compareTo(otherDate);
+        return dateToOrderBy.compareTo(otherDate);
+    }
+    
+    public Name getName() {      
+        return itemName;
+    }
+    
+    public Date getDateToOrderBy() {
+        return dateToOrderBy;
     }
 }
